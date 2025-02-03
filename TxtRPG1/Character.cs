db7386bdc8f8b1ef6f2bc805b3bc059369d58232
Kim-giseok <belbeck@naver.com>
2025-02-03 19:35:04 +0900

    캐릭터/상태보기
    캐릭터의 정보를 가진 클래스 제작
    캐릭터의 상태를 확인할 수 있는 메서드 제작

---
+++ TxtRPG1/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtRPG1
+{
+    internal class Character
+    {
+        int level;
+        string name;
+        enum Class { 전사, 궁수, 마법사 };
+        Class job;
+        int atk, def, hp, gold;
+
+        public Character(string name)
+        {
+            this.name = name;
+            level = 1;
+            job = Class.전사;
+            atk = 10;
+            def = 5;
+            hp = 100;
+            gold = 1500;
+        }
+
+        public void ShowStat()
+        {
+            Console.Clear();
+            Console.WriteLine($"Lv. {level:D2}");
+            Console.WriteLine($"{name} ({job})");
+            Console.WriteLine($"공격력 : {atk}");
+            Console.WriteLine($"방어력 : {def}");
+            Console.WriteLine($"체력 : {hp}");
+            Console.WriteLine($"Gold : {gold}");
+            Console.WriteLine();
+            Console.WriteLine($"0. 나가기");
+        }
+    }
+}
--- TxtRPG1/Program.cs
+++ TxtRPG1/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Character player = new Character("chad");
+            player.ShowStat();
         }
     }
 }
