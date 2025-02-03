dbb1dafefbddc1126a2f7937fa7e59fda4835270
Kim-giseok <belbeck@naver.com>
2025-02-03 20:01:42 +0900

    시작화면
    시작화면 출력을 구현

--- TxtRPG1/Program.cs
+++ TxtRPG1/Program.cs
@@ -4,9 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             Character player = new Character("chad");
-            player.ShowStat();
+
+            byte choice;
+            while (!StartScene(out choice))
+            { 
+                Console.WriteLine("잘못된 입력입니다");
+                Console.ReadKey();
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    player.ShowStat();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+
+        public static bool StartScene(out byte choice)
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+
+            Console.WriteLine();
+            Console.Write("원하시는 행동을 입력해주세요.\n>> ");
+            return byte.TryParse(Console.ReadLine(), out choice) && 1 <= choice && choice <= 3;
         }
     }
 }
