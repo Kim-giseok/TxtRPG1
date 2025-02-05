dafbd85d26a32f3f32ef458d1b6f8d2a096439df
Kim-giseok <belbeck@naver.com>
2025-02-05 13:02:26 +0900

    아이템 추가
    아이템 클래스를 참고해 강력한 무기를 하나 추가했습니다.

--- TxtRPG1/Program.cs
+++ TxtRPG1/Program.cs
@@ -16,12 +16,13 @@ namespace TxtRPG1
             Character player;
             Item[] items =
             {
-                    new Item("수련자　갑옷", Item.Type.armor, 5, "수련에　도움을　주는　갑옷입니다.", 1000),
-                    new Item("무쇠갑옷", Item.Type.armor, 9, "무쇠로　만들어져　튼튼한　갑옷입니다.", 2000),
-                    new Item("스파르타의　갑옷", Item.Type.armor, 15, "스파르타의　전사들이　사용했다는 전설의　갑옷입니다.", 3500),
-                    new Item("낡은　검", Item.Type.weapon, 2, "쉽게　볼　수　있는　낡은　검　입니다.", 600),
-                    new Item("청동　도끼", Item.Type.weapon, 5, "어디선가　사용됐던거　같은　도끼입니다.", 1500),
-                    new Item("스파르타의　창", Item.Type.weapon, 7, "스파르타의　전사들이　사용했다는　전설의　창입니다.", 3000)
+                new Item("수련자　갑옷", Item.Type.armor, 5, "수련에　도움을　주는　갑옷입니다.", 1000),
+                new Item("무쇠갑옷", Item.Type.armor, 9, "무쇠로　만들어져　튼튼한　갑옷입니다.", 2000),
+                new Item("스파르타의　갑옷", Item.Type.armor, 15, "스파르타의　전사들이　사용했다는 전설의　갑옷입니다.", 3500),
+                new Item("낡은　검", Item.Type.weapon, 2, "쉽게　볼　수　있는　낡은　검　입니다.", 600),
+                new Item("청동　도끼", Item.Type.weapon, 5, "어디선가　사용됐던거　같은　도끼입니다.", 1500),
+                new Item("스파르타의　창", Item.Type.weapon, 7, "스파르타의　전사들이　사용했다는　전설의　창입니다.", 3000),
+                new Item("종결무기", Item.Type.weapon, 10, "종결무기입니다.", 5000)
             };
             string name;
 
